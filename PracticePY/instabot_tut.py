1f8aa09fd559a598187ac29c6322a85df3253456
Tanmai2002 <tanmaikamat@gmail.com>
2021-10-26 11:21:39 +0530

    dwddImg created

--- PracticePY/instabot_tut.py
+++ PracticePY/instabot_tut.py
@@ -3,5 +3,9 @@ def postpic(pic_path):
     bot=instabot.Bot()
     bot.login()
     l=bot.upload_photo(pic_path)
+def downloadImg(url,name="test.jpg"):
+    import requests
+    r = requests.get(url, allow_redirects=True)
 
+    open(name, 'wb').write(r.content)
 
